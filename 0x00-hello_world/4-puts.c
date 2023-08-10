d481c888c865064e6dea7084b6a52edefa88c83b
Nas103 <tumelorhulani33@gmail.com>
2023-08-10 08:05:31 -0700

    code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - Code that execute a statement using the puts function
  *
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like a multilingual puzzle");
-	return (0);
+puts("\"Programming is like a multilingual puzzle");
+return (0);
 }

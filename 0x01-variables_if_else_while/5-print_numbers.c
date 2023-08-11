843ce9dce4ba08b944167e6f0ff6d70fc667a6b1
Nas103 <tumelorhulani33@gmail.com>
2023-08-11 15:53:46 -0700

    code

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,10 +10,9 @@
  */
 int main(void)
 {
-int number;
-for (number = 0; number <= 9; number++)
-{
-printf("%d\n", number);
-}
-return 0;
+int i;
+for (i = 0; i < 10; i++)
+printf("%d", i);
+printf("\n");
+return (0);
 }

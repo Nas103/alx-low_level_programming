99628b917ad51393566e52ff7cd7fc9534315f2e
Nas103 <tumelorhulani33@gmail.com>
2023-08-15 19:02:04 -0700

    c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauer - Prints every particular minute of the day of Jack Bauer 
+ * jack_bauer - Prints every particular minute of the day of Jack Bauer
  * Return: ...
  */
 
@@ -12,11 +12,12 @@ void jack_bauer(void)
 
 	for (a = 0; a <= 2; a++)
 	{
-	for (b = 0; b <= 9; a++)
+	for (b = 0; b <= 9; b++)
 	{
 	if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 	{
 	for (c = 0; c <= 5; c++)
+	{
 	for (d = 0; d <= 9; d++)
 	{
 	_putchar(a + '0');
@@ -25,6 +26,7 @@ void jack_bauer(void)
 	_putchar(c + '0');
 	_putchar(d + '0');
 	_putchar('\n');
+	}
 	}
 	}
 	}

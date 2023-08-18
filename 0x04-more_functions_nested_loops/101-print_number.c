21aad100ca682f641b57084cc2b456a7a992d4e8
Nas103 <tumelorhulani33@gmail.com>
2023-08-18 16:10:09 -0700

    c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,14 +8,16 @@
 void print_number(int n)
 
 {
+	unsigned int l = n;
+
 	if (n < 0)
 	{
-		_putchar('_');
-		n = -n;
+	n *= -1;
+	l = n;
+	_putchar('-');
 	}
-	if (n / 10 != 0)
-	{
-		print_number(n / 10);
-	}
-	_putchar((n % 10) + '0');
+	l /= 10;
+	if (l != 0)
+	print_number(l);
+	_putchar((unsigned int)n % 10 + '0');
 }

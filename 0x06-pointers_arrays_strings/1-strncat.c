da77795fa59d6a9e58c4900c070a75694d91a2e2
Nas103 <tumelorhulani33@gmail.com>
2023-08-24 11:19:09 -0700

    v

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,7 +8,7 @@
  * Return: dest
  */
 
-char *_strncat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	int j;
@@ -19,7 +19,7 @@ char *_strncat(char *dest, char *src)
 		i++;
 	}
 	j = 0;
-	while (src[j] != '\0')
+	while (j < n && src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;

02abcfdc5f7b4c312580a0400847c8a7fb0d917c
Nas103 <tumelorhulani33@gmail.com>
2023-11-23 22:32:39 -0800

    m bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,7 +16,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (a = 0; b[a]; a++)
 	{
-		if (b[a] < '0'|| b[a] > '1')
+		if (b[a] < '0' || b[a] > '1')
 			return (0);
 		dec_val = 2 * dec_val + (b[a] - '0');
 	}

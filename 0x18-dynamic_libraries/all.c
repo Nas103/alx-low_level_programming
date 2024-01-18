1e63280433bc206f00f74684e0fe78a2845223f6
Nas103 <tumelorhulani33@gmail.com>
2024-01-18 06:18:25 -0800

    dyc

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * div - divides int
+ * @: 1st int
+ * @b 2nd int
+ * Return: output
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - mod int
+ * @: 1st int
+ * @b 2nd int
+ * Return: output
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+/**
+ * sub - sub int
+ * @: 1st int
+ * @b 2nd int
+ * Return: output
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiply int
+ * @: 1st int
+ * @b 2nd int
+ * Return: output
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * add - add int
+ * @: 1st int
+ * @b 2nd int
+ * Return: output
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
